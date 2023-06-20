cc137d501f1a62840f400c3bcb2a1215aecdabb5
QuangHuy2806 <quockhanh82599@gmail.com>
2023-06-20 15:14:51 +0700

    update

---
+++ Connection.php
@@ -0,0 +1,4 @@
+<?php
+    $conn = mysqli_connect('dfkpczjgmpvkugnb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'g7f2khlulelhxsf8', 'ip8l72bdhksjess4','e54wk6oj0qhhbbyw')
+        or die("Can not connect database" . mysqli_connect_error());
+?>
